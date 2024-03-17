ba6dc06fe8f1674fbf3ddf144ce3cc3165142264
Ilsa. <ilsasyeda@gmail.com>
2024-03-17 17:01:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function ShirtDetails(color, size, print) {
+    if (print === void 0) { print = "shark"; }
+    console.log("The color of the T-shirt is ".concat(color, " with ").concat(size, " size and ").concat(print, " print."));
+}
+;
+ShirtDetails("black", 25);
---
+++ main.ts
@@ -0,0 +1,5 @@
+function ShirtDetails(color: string, size: number, print: string = "shark" ){
+    console.log(`The color of the T-shirt is ${color} with ${size} size and ${print} print.`)
+};
+ShirtDetails("black", 25);
+                        // shark becomes the default value incase if no value is provided
